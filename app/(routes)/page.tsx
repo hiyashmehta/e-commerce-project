0a32c6c6a8eb407fee77ee47e0f36bfa41a2e99c
Yash Mehta <yashmehta0395@gmail.com>
2023-11-21 17:43:37 +0530

    Created new component named product-list

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,18 +1,27 @@
-import Container from '@/components/ui/container'
-import React from 'react'
+import getBillboard from "@/actions/get-billboard";
+import getProducts from "@/actions/get-products";
+import ProductList from "@/components/product-list";
+import Billboard from "@/components/ui/billboard";
+import Container from "@/components/ui/container";
 
-const HomePage = () => {
+export const revalidate = 0;
+
+const HomePage = async () => {
+  const products = await getProducts({ isFeatured: true });
+  const billboard = await getBillboard("c51bbe22-54d4-4953-8263-0a846582960b");
+
   return (
     <Container>
-      <div className='space-y-10 pb-10'>
-        Billboard
-      
-      <div className='flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8'>
-        ProductList
-      </div>
+      <div className="space-y-10 pb-10">
+        <Billboard 
+          data={billboard}
+        />
+        <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+          <ProductList title="Featured Products" items={products} />
+        </div>
       </div>
     </Container>
   )
-}
+};
 
-export default HomePage
+export default HomePage;
